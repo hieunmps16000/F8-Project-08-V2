8a044e650343e41acc913e7bfa249b274c13763d
Nguyen Minh Hieu <lotous12a4@gmail.com>
2026-02-03 11:03:57 +0700

    Làm phần nội dung bài viết #3

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -174,7 +174,7 @@ function initJsToggle() {
                     button.click();
                 }
             }
-        };        
+        };
     });
 }
 
@@ -187,5 +187,27 @@ window.addEventListener("template-loaded", () => {
             const item = link.closest("li");
             item.classList.toggle("navbar__item--active");
         };
+    });
+});
+
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "tab__item";
+    const contentsSelector = "tab__content";
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
     });
 });
